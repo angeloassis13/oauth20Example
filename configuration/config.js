2090f286496aa0f7e600509d19a5c22c1f98930f
Angelo Assis <angeloassis13@gmail.com>
2020-07-12 20:15:50 -0300

    Simple API with Google OAuth 2.0

---
+++ configuration/config.js
@@ -0,0 +1,5 @@
+module.exports={
+    "api_key": "57854745605-6vt481gp1hroemim6gfgvahdi0rulobc.apps.googleusercontent.com",
+    "api_secret": "zAOS2syBv9w7cg1dl8AQIYqM",
+    "callback_url": "http://localhost:3000/auth/google/callback"
+}
